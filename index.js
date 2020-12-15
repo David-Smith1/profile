4413a18fe00fb8266fed48b4bb3d22edf88d25e6
David Smith <smitty6000@gmail.com>
2020-12-15 22:20:25 +0900

    nav button dropdown

--- index.js
+++ index.js
@@ -3,9 +3,5 @@ const dropdown = document.querySelector(".navigation-extendable")
 
 dropdownButton.addEventListener("click", (event) => {
     event.preventDefault();
-    if (dropdown.style.height = 150); {
-        dropdown.style.height = 0;
-    } else {
-        dropdown.style.height = 150;
-    }
+    if (dropdown.style.height = 150 ? dropdown.style.height = 0 : dropdown.style.height = 150);
 });
