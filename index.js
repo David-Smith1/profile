eefc878a2e080e9530ca4d0b75105c7943a824be
David Smith <smitty6000@gmail.com>
2020-12-17 23:30:22 +0900

    fixed links, a tag size

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const dropdown = document.querySelector(".navigation-extendable")
 
 dropdownButton.addEventListener("click", (event) => {
     event.preventDefault();
-    if (dropdown.style.height = 150 ? dropdown.style.height = 0 : dropdown.style.height = 150);
+    dropdown.classList.toggle("navigation-extendable-2");
 });
+
