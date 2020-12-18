4308bd275f5c06f7af307b353499ca8c26931bd2
David Smith <smitty6000@gmail.com>
2020-12-18 21:54:05 +0900

    fixed script, drop down button style finishedd

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const dropdown = document.querySelector(".navigation-extendable")
 dropdownButton.addEventListener("click", (event) => {
     event.preventDefault();
     dropdown.classList.toggle("navigation-extendable-2");
+    (dropdown.classList.contains("navigation-extendable-2")) ? dropdownButton.style.color = "white" : dropdownButton.style.color = null;
+
 });
 
